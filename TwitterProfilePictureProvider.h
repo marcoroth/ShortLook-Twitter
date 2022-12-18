bb68ea28eaee76315459cbf92871a2485ce7ed6c
Marco Roth <marco.roth@intergga.ch>
2022-12-18 20:32:22 +0100

    Up

---
+++ TwitterProfilePictureProvider.h
@@ -0,0 +1,5 @@
+#import "ShortLook-API.h"
+
+@interface TwitterProfilePictureProvider : NSObject <DDNotificationContactPhotoProviding>
+- (DDNotificationContactPhotoPromiseOffer *)contactPhotoPromiseOfferForNotification:(DDUserNotification *)notification;
+@end
